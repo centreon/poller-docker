a4c83af0074f1afb194f1264783dbdfd5e61ca71
Sylvestre Gallon <ccna.syl@gmail.com>
2020-03-01 17:06:25 +0100

    chore(build.py): get a skeleton for docker build

--- build.py
+++ build.py
@@ -1,15 +1,106 @@
 #!/bin/env python
 import os
+import subprocess
 import json
 
-with open('config.json', 'r') as f:
-    config = json.load(f)
-    version = config['version']
-    git_rev = config['git_tag']
-    distro = config['distribution']
-    path = os.path.realpath(__file__) 
-    print('poller ' + version + ' using git rev ' + git_rev + ' on ' + distro)
-    print('working dir : ' + path)
+class Debug:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    def info(self, msg):
+        print('[' + self.OKGREEN + 'info' + self.ENDC + '] ' + msg)
+
+    def error(self, msg):
+        print('[' + self.FAIL + 'error' + self.ENDC + '] ' + msg)
+
+
+class Docker:
+    def __init__(self, version, git_rev, distro, work_path):
+        self.version = version
+        self.git_rev = git_rev
+        self.distro = distro
+        self.dbg = Debug()
+        self.work_path = work_path
+
+        self.dbg.info('poller ' + self.dbg.OKBLUE + version + self.dbg.ENDC + '@' + self.dbg.OKBLUE
+                 + git_rev + self.dbg.ENDC + ' on ' + self.dbg.OKBLUE + distro + self.dbg.ENDC)
+
+        self.dbg.info('working dir : ' + self.dbg.OKBLUE + self.work_path + self.dbg.ENDC)
+
+    def __launch(self, args_array):
+        process = subprocess.Popen(args_array, stdout=subprocess.PIPE, universal_newlines=True)
+        return_code = None
+        while True:
+            output = process.stdout.readline()
+            print(output.strip())
+            # Do something else
+            return_code = process.poll()
+            if return_code is not None:
+                # Process has finished, read rest of the output
+                for output in process.stdout.readlines():
+                    print(output.strip())
+                break
+        return return_code
+
+    def build(self, name):
+        self.dbg.info('starting build phase for ' + self.dbg.OKBLUE + name + self.dbg.ENDC)
+
+        os.chdir(self.work_path + '/' + name + '/build/' + self.distro)
+        return_code = self.__launch(['docker', 'build', '-t', name + '-' + self.version + ':' + self.distro, '.'])
+        os.chdir(self.work_path)
+
+        if return_code == 0:
+            self.dbg.info('build done')
+            return True
+        else:
+            self.dbg.error('build fail with error code: ' + self.dbg.WARNING + str(return_code) + self.dbg.ENDC)
+            return False
+
+    def test(self, name):
+        self.dbg.info('starting test phase for ' + self.dbg.OKBLUE + name + self.dbg.ENDC)
+
+        os.chdir(self.work_path + '/' + name + '/test/' + self.distro)
+        return_code = self.__launch(['docker', 'build', '-t', name + '-' + self.version + '-test:' + self.distro, '.'])
+        os.chdir(self.work_path)
+
+        if return_code == 0:
+            self.dbg.info('build done')
+            return True
+        else:
+            self.dbg.error('build fail with error code: ' + self.dbg.WARNING + str(return_code) + self.dbg.ENDC)
+            return False
+
+    def package(self, name):
+        self.dbg.info('starting package phase for ' + self.dbg.OKBLUE + name + self.dbg.ENDC)
+
+        os.chdir(self.work_path + '/' + name + '/package/' + self.distro)
+        return_code = self.__launch(['docker', 'build', '-t', name + '-' + self.version + '-test:' + self.distro, '.'])
+        os.chdir(self.work_path)
+
+        if return_code == 0:
+            self.dbg.info('build done')
+            return True
+        else:
+            self.dbg.error('build fail with error code: ' + self.dbg.WARNING + str(return_code) + self.dbg.ENDC)
+            return False
+
+if __name__ == '__main__':
+    config = None
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    dock = Docker(config['version'], config['git_tag'], config['distribution'], os.path.dirname(os.path.realpath(__file__)))
 
     for proj in config['build']:
-        print(proj)
+        if proj['build'] == True:
+            dock.build(proj['name'])
+        if proj['test'] == True:
+            dock.test(proj['name'])
+        if proj['package'] == True:
+            dock.package(proj['name'])
